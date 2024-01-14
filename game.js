c3fa2102ee0b7be27e77074f71c81b271611f4e6
Pratik <patilpratikp035@gmail.com>
2024-01-14 14:50:19 +0530

    adding js code

--- game.js
+++ game.js
@@ -1,2 +1,44 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset-btn");
 
+let turnX = true;
 
+let winPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box was clickled");
+    if (turnX) {
+      box.innerHTML = "X";
+      turnX = false;
+    } else {
+      box.innerHTML = "O";
+      turnX = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+  for (pattern of winPattern) {
+    let posVal1 = boxes[pattern[0]].innerText;
+    let posVal2 = boxes[pattern[1]].innerText;
+    let posVal3 = boxes[pattern[2]].innerText;
+
+    if (posVal1 != "" && posVal2 != "" && posVal3 != "") {
+      if (posVal1 === posVal2 && posVal2 === posVal3) {
+        console.log("winner", posVal1);
+      }
+    }
+  }
+};
